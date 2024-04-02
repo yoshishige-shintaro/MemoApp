a037c1925cd64e0e8a0af381d5db2aac084fa935
yoshishige-shintaro <yoshishige.shintaro@gmail.com>
2024-04-02 22:31:25 +0900

    expo-router の導入

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+export default Index;
