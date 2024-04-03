2f2d3fba8e965da963fc7a4ee32bc0e012feebd3
yoshishige-shintaro <yoshishige.shintaro@gmail.com>
2024-04-03 21:04:11 +0900

    ログイン状態の監視

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,23 @@
 import { StyleSheet } from "react-native";
 
-import { Redirect } from "expo-router";
+import { Redirect, router } from "expo-router";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config";
 
 const Index = (): JSX.Element => {
-  return <Redirect href={"auth/sign_up"} />;
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      // ユーザーが存在しない場合は早期リターン
+      if (!user) {
+        return;
+      }
+
+      router.replace("memo/list");
+    });
+  }, []);
+
+  return <Redirect href={"auth/login"} />;
 };
 
 const styles = StyleSheet.create({});
