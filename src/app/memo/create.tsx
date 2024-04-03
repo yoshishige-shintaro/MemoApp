204f3deaa07573bc9e0b54a667f02cae4228ca30
yoshishige-shintaro <yoshishige.shintaro@gmail.com>
2024-04-03 21:24:49 +0900

    firestore にデータを保存する処理を実装

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -2,19 +2,49 @@ import { StyleSheet, TextInput, View, KeyboardAvoidingView } from "react-native"
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
 import { router } from "expo-router";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+import { auth, db } from "../../config";
+import { useState } from "react";
 
-const handlePressCircleButton = (): void => {
-  router.back();
+const handlePressCircleButton = (bodyText: string): void => {
+  if (!auth.currentUser) {
+    return;
+  }
+  const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+  addDoc(ref, {
+    title: "",
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date()),
+  })
+    .then((docRef) => {
+      console.log(docRef.id);
+      router.back();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const Create = (): JSX.Element => {
+  const [bodyText, setBodyText] = useState("");
   return (
     <KeyboardAvoidingView style={styles.container} behavior="height">
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} multiline value={""} />
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => {
+            setBodyText(text);
+          }}
+          multiline
+          value={bodyText}
+        />
       </View>
 
-      <CircleButton onPress={handlePressCircleButton}>
+      <CircleButton
+        onPress={() => {
+          handlePressCircleButton(bodyText);
+        }}
+      >
         <Icon name="check" size={40} color="#ffffff" />
       </CircleButton>
     </KeyboardAvoidingView>
