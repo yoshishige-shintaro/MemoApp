fe6ebfbcadb5453da6bc470c4c6aaeb76bcce2e5
yoshishige-shintaro <yoshishige.shintaro@gmail.com>
2024-04-04 00:50:58 +0900

    メモ削除機能の実装

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,11 +1,32 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
 import Icon from "./Icon";
 import { Link } from "expo-router";
 import { type Memo } from "../../types/memo";
+import { auth, db } from "../config";
+import { deleteDoc, doc } from "firebase/firestore";
 
 interface Props {
   memo: Memo;
 }
+
+const handlePressDelete = (id: string): void => {
+  if (!auth.currentUser) {
+    return;
+  }
+  const ref = doc(db, `users/${auth.currentUser.uid}/memos`, String(id));
+  Alert.alert("メモを削除します", "この操作はもとに戻せません", [
+    {
+      text: "キャンセル",
+    },
+    {
+      text: "削除する",
+      style: "destructive",
+      onPress() {
+        deleteDoc(ref).catch(() => Alert.alert("削除に失敗しました"));
+      },
+    },
+  ]);
+};
 
 const MemoListItem = (props: Props): JSX.Element | null => {
   const { memo } = props;
@@ -23,7 +44,11 @@ const MemoListItem = (props: Props): JSX.Element | null => {
           </Text>
           <Text style={styles.memoListItemDate}>{dateString}</Text>
         </View>
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            handlePressDelete(memo.id);
+          }}
+        >
           <Icon name="delete" size={40} color="#878686" />
         </TouchableOpacity>
       </TouchableOpacity>
